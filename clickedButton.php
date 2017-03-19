8e14b642ad413cdbba7d2fee8797c4c7d20d0569
Josh Byster <joshb1050@gmail.com>
2017-03-19 02:18:06 -0500

    fixed (what seems to be) any issues with autodraft"

--- clickedButton.php
+++ clickedButton.php
@@ -10,42 +10,54 @@ include('dbinfo.php');
      die("Connection failed: " . $conn->connect_error);
  }
 
- $sql = "UPDATE students SET taken_by='".$name. "' WHERE id=".$id;
- $sql2 = "SELECT * FROM teachers WHERE name='".$name."'";
+$sql = "UPDATE students SET taken_by='".$name. "' WHERE id=".$id;
+$sql2 = "SELECT * FROM teachers WHERE name='".$name."'";
 $broad_teacher_select= "SELECT MIN(id) as MINID, MAX(id) as MAXID FROM teachers";
- $conn->query($sql);
- $result_teachers = $conn->query($sql2); //gets teacher id
- $range= $conn->query($broad_teacher_select); //gets min max values
- $range_values=$range->fetch_assoc();
- $min = $range_values['MINID'];
- $max=$range_values['MAXID'];
+$conn->query($sql);
+$result_teachers = $conn->query($sql2); //gets teacher id
+$range= $conn->query($broad_teacher_select); //gets min max values
+$range_values=$range->fetch_assoc();
+$min = $range_values['MINID'];
+$max=$range_values['MAXID'];
+$current_teacher_id=intval($result_teachers->fetch_assoc()['id']);
 
- $current_teacher_id=intval($result_teachers->fetch_assoc()['id']);
- $next_teacher_id=$current_teacher_id+1;
-$conn->query("UPDATE teachers SET active=0 WHERE id=".$current_teacher_id);
-if ($current_teacher_id>=$max) {
-    $next_teacher_id=$min;
+function getNextTeacher(&$current_teacher_id, $min, $max, $conn)
+{
+    $conn->query("UPDATE teachers SET active=0 WHERE id=".$current_teacher_id);
+    $current_teacher_id=$current_teacher_id+1;
+    echo("Current teacher id is: ".$current_teacher_id);
+    if ($current_teacher_id>$max) {
+          echo("But max is: ".$max);
+        $current_teacher_id=$min;
+        echo("Now current teacher id is ".$current_teacher_id);
+    }
+    $resultant=$conn->query("UPDATE teachers SET active=1 WHERE id=".$current_teacher_id);
 }
-$conn->query("UPDATE teachers SET active=1 WHERE id=".$next_teacher_id);
+getNextTeacher($current_teacher_id, $min, $max, $conn);
 
-$list_of_arr=$conn->query("SELECT * FROM teachers WHERE id>=".$next_teacher_id);
-$k = $list_of_arr->fetch_assoc();
-while(isset($k)){
+$k=$conn->query("SELECT * FROM teachers WHERE id=".$current_teacher_id)->fetch_assoc();
+while ($k['arr']!='0'||$k['arr']!=0) {
     $choice_array=unserialize($k['arr']);
     foreach ($choice_array as $j) {
-      echo("RUnning a foreach");
         $matched_query=$conn->query("SELECT * FROM students WHERE taken_by='' AND name LIKE '%".$j."%'");
+        //print($matched_query);
         $matched_row=$matched_query->fetch_assoc();
         if (isset($matched_row)) {
             $qu="UPDATE students SET taken_by='".$k['name']."' WHERE id='".$matched_row['id']."'";
-            echo($qu);
+            print($qu);
             $conn->query($qu);
             break;
+            echo("breaking");
         }
-      }
-      print("Changing the value of k");
-    $k=$list_of_arr->fetch_assoc();
+    }
+    getNextTeacher($current_teacher_id, $min, $max, $conn);
+    print("TESTING");
+    $k=$conn->query("SELECT * FROM teachers WHERE id=".$current_teacher_id)->fetch_assoc();
+    echo($current_teacher_id);
 }
+print("Changing the value of k");
+$k=$list_of_arr->fetch_assoc();
+
 $conn->close();
 
  ?>
